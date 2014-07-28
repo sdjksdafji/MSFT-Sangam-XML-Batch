74dcd78828d5e8a32c059554760efffc143169c3
Shuyi-MSFT <sdjksdafji@gmail.com>
2014-07-28 13:35:52 -0700

    dfs printing finished

--- SamganXmlBatch/SamganXmlBatch/Program.cs
+++ SamganXmlBatch/SamganXmlBatch/Program.cs
@@ -12,12 +12,16 @@ namespace SamganXmlBatch
         static void Main(string[] args)
         {
 
-            //D:\SearchGold\deploy\builds\data\Sangam_Partners\Sangam-Prod2\OfficeWAC\TestEnv\Jobs\EventTags.job.xml
+            //D:\SearchGold\deploy\builds\data\Sangam_Partners\Sangam-Prod2\OfficeWAC\TestEnv\Jobs\EventTags.job - Copy.xml
             String path = getPathFromConsole();
 
             XmlDocument doc = new XmlDocument();
             doc.Load(path);
-            Console.WriteLine(doc.InnerText);
+
+            Console.WriteLine(doc.ChildNodes.Count);
+
+            dfsPrintNameAndInnerText(doc, 0);
+
 
             Console.ReadKey();
         }
@@ -31,6 +35,23 @@ namespace SamganXmlBatch
                 Console.WriteLine("The path is: " + path);
             }
             return path;
+        }
+
+        static private void dfsPrintNameAndInnerText(XmlNode node, int depth)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            for (int i = 0; i < depth; i++)
+            {
+                Console.Write("\t");
+            }
+            Console.WriteLine("Name: " + node.Name + " || Inner Text: " + node.InnerText);
+            for (int i = 0; i < node.ChildNodes.Count; i++)
+            {
+                dfsPrintNameAndInnerText(node.ChildNodes[i], depth + 1);
+            }
         }
     }
 }
