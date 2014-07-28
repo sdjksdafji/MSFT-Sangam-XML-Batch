080681900541104351c4038aa0c47a906ab63930
Shuyi-MSFT <sdjksdafji@gmail.com>
2014-07-28 11:53:21 -0700

    project created

---
+++ SamganXmlBatch/SamganXmlBatch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamganXmlBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path of XML file:");
+            Console.WriteLine();
+            Console.Write("   ");
+            String line = Console.ReadLine();
+            if (line != null)
+            {
+                Console.WriteLine("The path is: " + line);
+            }
+            Console.ReadKey();
+        }
+    }
+}
